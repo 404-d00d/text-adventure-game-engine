1376cebb2dea787ea26326092d45fc56e93e45d7
404-d00d <67910702+404-d00d@users.noreply.github.com>
2026-03-13 23:53:27 -0400

    Update WorldObject.py

--- WorldObject.py
+++ WorldObject.py
@@ -1,83 +1,118 @@
 class WorldObject:
-	# ID = double, invetory = array/dictionary
-	def __init__(self, inventory, ID, description, inspect, interaction):
-		self.inventory = inventory
-		self.ID = ID
-		self.description = description
-		self.inspect = inspect
-		self.interaction = interaction
-		self.options = ("-----\n"
-					  "1. Look Closer\n"
-					  "2. Interact\n"
-					  "3. Show Inventory\n"
-					  "4. Add to Inventory\n"
-					  "Any Other Option. Do Nothing")
+    def __init__(self, Inventory, Id, Description, Inspect, Interaction, IsPassable=False):
+        self.Inventory = list(Inventory)
+        self.Id = Id
+        self.Description = Description
+        self.Inspect = Inspect
+        self.Interaction = Interaction
+        self.IsPassable = IsPassable
+        self.Options = (
+            "-----\n"
+            "1. Look Closer\n"
+            "2. Interact\n"
+            "3. Show Inventory\n"
+            "4. Add To Inventory\n"
+            "Any Other Option.\n"
+            "Do Nothing"
+        )
 
-	def getID(self):
-		return self.ID
+    def GetId(self):
+        return self.Id
 
-	def getInventory(self):
-		return self.inventory
+    def GetDisplayId(self):
+        return self.Id
 
-	def showInventory(self):
-		print("===")
-		print("OBJECT INVENTORY")
-		for x in range(len(self.inventory)):
-			print(str(x)+": "+str(self.inventory[x].getQuantity())+" "+self.inventory[x].getName())
+    def GetInventory(self):
+        return self.Inventory
 
-	def lootInventory(self, player):
-		selection = ""
-		while selection != "e":
-			try:
-				self.showInventory()
-				print("e: exit inventory")
-				selection = input("Choose your option: ")
-				selection = int(selection)
-				print(self.inventory[selection].getName()+" is the item you selected.")
-				player.addItem(self.inventory[selection])
-				self.inventory.pop(selection)
-			except (ValueError, IndexError):
-				print("This option is not valid.")
-		return ("You are done with this item")
+    def GetIsPassable(self):
+        return self.IsPassable
 
-	def placeIntoInventory(self, player):
-		selection = ""
-		while selection != "e":
-			try:
-				player.showInventory()
-				print("e: exit inventory")
-				selection = input("Choose your option: ")
-				selection = int(selection)
-				print(player.getInventory()[selection].getName()+" is the item you put into the object")
-				self.inventory.append(player.showInventory()[selection])
-				player.removeItem(self.inventory[selection])
-			except (ValueError, IndexError):
-				print("This option is not valid.")
-		return ("You are done with this item")
+    def SetIsPassable(self, NewState):
+        self.IsPassable = NewState
 
-	def showDescription(self):
-		return self.description
+    def FormatInventory(self):
+        Lines = ["===", "OBJECT INVENTORY"]
+        if not self.Inventory:
+            Lines.append("(empty)")
+        else:
+            for Index, ItemObject in enumerate(self.Inventory):
+                Lines.append(
+                    str(Index)
+                    + ": "
+                    + str(ItemObject.GetQuantity())
+                    + " "
+                    + ItemObject.GetName()
+                )
+        return "\n".join(Lines)
 
-	def showOptions(self):
-		return self.options
+    def ShowInventory(self):
+        print(self.FormatInventory())
 
-	# 1 = look closer
-	# 2 = interact
-	# 3 = loot inventory
-	# 4 = transfer into item
-	# anything else = do nothing
-	def interact(self, response, player):
-		if response == "1":
-			return (self.inspect)
-		elif response == "2":
-			return (self.interaction)
-		elif response == "3":
-			return (self.lootInventory(player))
-		elif response == "4":
-			return (self.placeIntoInventory(player))
-		else:
-			return ("You do nothing")
+    def LootInventory(self, Player, InputFunction=input, OutputFunction=print):
+        if not self.Inventory:
+            return "There is nothing inside."
 
-	# call method if you need to create the same object with a different inventory
-	def createUnique(self, diffInventory):
-		return WorldObject(diffInventory, self.ID, self.description, self.inspect, self.interaction)
+        while True:
+            OutputFunction(self.FormatInventory())
+            OutputFunction("e: Exit Inventory")
+            Selection = InputFunction("Choose Your Option: ").strip()
+
+            if Selection.lower() == "e":
+                return "You are done with this item."
+
+            try:
+                SelectionIndex = int(Selection)
+                ItemObject = self.Inventory.pop(SelectionIndex)
+                OutputFunction(ItemObject.GetName() + " is the item you selected.")
+                Player.AddItem(ItemObject)
+            except (ValueError, IndexError):
+                OutputFunction("This option is not valid.")
+
+    def PlaceIntoInventory(self, Player, InputFunction=input, OutputFunction=print):
+        if not Player.GetInventory():
+            return "You have nothing to place into this object."
+
+        while True:
+            OutputFunction(Player.FormatInventory())
+            OutputFunction("e: Exit Inventory")
+            Selection = InputFunction("Choose Your Option: ").strip()
+
+            if Selection.lower() == "e":
+                return "You are done with this item."
+
+            try:
+                SelectionIndex = int(Selection)
+                ItemObject = Player.RemoveItemAt(SelectionIndex)
+                self.Inventory.append(ItemObject)
+                OutputFunction(ItemObject.GetName() + " is the item you put into the object.")
+            except (ValueError, IndexError):
+                OutputFunction("This option is not valid.")
+
+    def ShowDescription(self):
+        return self.Description
+
+    def ShowOptions(self):
+        return self.Options
+
+    def Interact(self, Response, Player, InputFunction=input, OutputFunction=print):
+        if Response == "1":
+            return self.Inspect
+        if Response == "2":
+            return self.Interaction
+        if Response == "3":
+            return self.LootInventory(Player, InputFunction, OutputFunction)
+        if Response == "4":
+            return self.PlaceIntoInventory(Player, InputFunction, OutputFunction)
+        return "You do nothing."
+
+    def CreateUnique(self, DifferentInventory):
+        CreatedInventory = [ItemObject.CreateUnique() for ItemObject in DifferentInventory]
+        return WorldObject(
+            CreatedInventory,
+            self.Id,
+            self.Description,
+            self.Inspect,
+            self.Interaction,
+            self.IsPassable,
+        )
